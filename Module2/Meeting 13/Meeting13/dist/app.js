7298b1ecae11425dd1779d663a15621c7f7c5d8e
Coral Tzubery <tzubery.coral@gmail.com>
2025-01-12 18:32:31 -0800

    ordering the moudle 2's files.

--- Module2/Meeting 13/Meeting13/dist/app.js
+++
@@ -1,5 +0,0 @@
-var food = prompt("What is your favorite food?");
-alert("Wow! " + food + " is my favoite too!");
-var num1 = Number(prompt("Insert a number"));
-var num2 = Number(prompt("Insert another number"));
-alret(num1 + num2);
